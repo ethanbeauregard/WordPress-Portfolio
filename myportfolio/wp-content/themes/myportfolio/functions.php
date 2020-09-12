be111ccef8ba8420d6fd6cc36ed5120be9a5b872
Ethan B <ethantbeauregard@gmail.com>
2020-09-12 03:15:53 -0500

    displayed feature images with pods and looped over projects in project section

--- myportfolio/wp-content/themes/myportfolio/functions.php
+++ myportfolio/wp-content/themes/myportfolio/functions.php
@@ -4,4 +4,4 @@ function mytheme_post_thumbnails() {
   add_theme_support('post-thumbnails');
 }
 
-add_action('after_theme_setup', 'mytheme_post_thumbnails');
+add_action('after_setup_theme', 'mytheme_post_thumbnails');
